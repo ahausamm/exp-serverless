3d45e33acb71f0566866585e1fee122acdbd6602
ahausamm <ahausamm@Adrians-MacBook-Pro.local>
2016-09-20 23:16:15 +0200

    added greet - API

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@
 
 // Your first function handler
 module.exports.hello = (event, context, cb) => cb(null,
-  { message: 'Go Serverless v1.0! Your function executed successfully!', event }
+  { message: 'Go Serverless from Adrian' }
 );
 
 // You can add more handlers here, and reference them in serverless.yml
